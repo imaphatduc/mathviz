b0d6cc489b9deca1b5bee2120bec8dc3ca50851d
imaphatduc <vutranducphat@gmail.com>
2024-01-25 23:18:32 +0700

    no base

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import ChaosSimulator from './routes/chaos-simulator';
 import GameOfLife from './routes/game-of-life-Conway';
 
 export default function App() {
-  const base = '/mathviz';
-
   return (
     <div>
-      <Route path={`${base}/chaos-simulator`}>
+      <Route path="/chaos-simulator">
         <ChaosSimulator />
       </Route>
-      <Route path={`${base}/game-of-life-Conway`}>
+      <Route path="/game-of-life-Conway">
         <GameOfLife />
       </Route>
     </div>
