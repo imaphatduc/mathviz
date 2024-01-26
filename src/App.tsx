6480508b3444bd0414bbd1b5a71bf15c3fee08dc
imaphatduc <vutranducphat@gmail.com>
2024-01-26 11:26:34 +0700

    change base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Route } from 'wouter';
+import { Route, Router } from 'wouter';
 import ChaosSimulator from './routes/chaos-simulator';
 import GameOfLife from './routes/game-of-life-Conway';
 
 export default function App() {
   return (
-    <div>
+    <Router base="/mathviz">
       <Route path="/chaos-simulator">
         <ChaosSimulator />
       </Route>
       <Route path="/game-of-life-Conway">
         <GameOfLife />
       </Route>
-    </div>
+    </Router>
   );
 }
