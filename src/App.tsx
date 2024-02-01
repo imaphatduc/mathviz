14a2515f0008f9c8e282849beadf6172b9b2bae9
Duc Phat <51194912+imaphatduc@users.noreply.github.com>
2024-02-01 11:22:59 +0700

    enable analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route } from 'wouter';
 import ChaosSimulator from './routes/chaos-simulator';
 import GameOfLife from './routes/game-of-life-Conway';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/game-of-life-Conway">
         <GameOfLife />
       </Route>
+      <Analytics />
     </>
   );
 }
